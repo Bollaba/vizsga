3c858ffdb7efbdc197b8aa45e47b54bec518fed5
HQUVSZ <boroska07@gmail.com>
2024-11-27 09:26:03 +0100

    SzemelyAuto osztály elkészítése

---
+++ Vizsga/SzemelyAuto.py
@@ -0,0 +1,15 @@
+from Auto import Auto
+class SzemelyAuto(Auto):
+    def __init__(self, rendszam, dij):
+        super().__init__(rendszam, dij)
+        self.extras = []
+    def book_auto(self):
+        if not self.is_booked:
+            self.is_booked = True
+        else:
+            print("Hiba, az autó már foglalt!")
+    def unbook_auto(self):
+        if self.is_booked:
+            self.is_booked = False
+        else:
+            print("Hiba, az autó nem is volt foglalt!")
