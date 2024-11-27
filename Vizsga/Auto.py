9244ba0f34dd0c3206909eb5fdb8fcb5ce09cb85
HQUVSZ <boroska07@gmail.com>
2024-11-27 15:28:29 +0100

    Main osztály elkészítése

--- Vizsga/Auto.py
+++ Vizsga/Auto.py
@@ -1,11 +1,13 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 class Auto(ABC):
     def __init__(self, rendszam, dij):
         self.rendszam = rendszam
         self.dij = dij
         self.is_booked = False
         self.extras = []
+    @abstractmethod
     def book_auto(self):
         pass
+    @abstractmethod
     def unbook_auto(self):
         pass
